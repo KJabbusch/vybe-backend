4058c394865faf1d40ec8a2a3b0a4508c5aca354
Kristel Jabbusch <kaj12@my.fsu.edu>
2022-02-03 14:11:25 -0800

    final auth works!!!!

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,20 @@
-use rspotify::{
-    model::{AdditionalType, Country, Market},
-    prelude::*,
-    scopes, AuthCodeSpotify, Credentials, OAuth,
-};
 
-#[tokio::main]
-async fn main() {
+
+use rspotify::{model::AlbumId, prelude::*, ClientCredsSpotify, Credentials};
+
+fn main() {
     let creds = Credentials::from_env().unwrap();
-    let oauth = OAuth::from_env(scopes!("user-read-currently-playing")).unwrap();
-    
-    let mut spotify = AuthCodeSpotify::new(creds, oauth);
 
-    // // Obtaining the access token
-    let url = spotify.get_authorize_url(false).unwrap();
-    spotify.prompt_for_token(&url).unwrap();
-    
-    // // Running the requests
-    let market = Market::Country(Country::UnitedStates);
-    let additional_types = [AdditionalType::Episode];
-    let artists = spotify
-        .current_playing(Some(&market), Some(&additional_types));
+    let mut spotify = ClientCredsSpotify::new(creds);
 
-    println!("Response: {:#?}", artists);
+    // Obtaining the access token. Requires to be mutable because the internal
+    // token will be modified. We don't need OAuth for this specific endpoint,
+    // so `...` is used instead of `prompt_for_user_token`.
+    spotify.request_token().unwrap();
+
+    // Running the requests
+    let birdy_uri = AlbumId::from_uri("spotify:album:0sNOF9WDwhWunNAHPD3Baj").unwrap();
+    let albums = spotify.album(&birdy_uri);
+
+    println!("Response: {:#?}", albums);
 }
