0ef7606c6c42b35c0177096e38d2eb395e5e83d8
Kristel Jabbusch <kaj12@my.fsu.edu>
2022-02-03 13:25:48 -0800

    one step closer to authentication!

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,25 @@
-#[macro_use] extern crate rocket;
+use rspotify::{
+    model::{AdditionalType, Country, Market},
+    prelude::*,
+    scopes, AuthCodeSpotify, Credentials, OAuth,
+};
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
-}
+#[tokio::main]
+async fn main() {
+    let creds = Credentials::from_env().unwrap();
+    let oauth = OAuth::from_env(scopes!("user-read-currently-playing")).unwrap();
+    
+    let mut spotify = AuthCodeSpotify::new(creds, oauth);
 
-#[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    // // Obtaining the access token
+    let url = spotify.get_authorize_url(false).unwrap();
+    spotify.prompt_for_token(&url).unwrap();
+    
+    // // Running the requests
+    let market = Market::Country(Country::UnitedStates);
+    let additional_types = [AdditionalType::Episode];
+    let artists = spotify
+        .current_playing(Some(&market), Some(&additional_types));
+
+    println!("Response: {:#?}", artists);
 }
